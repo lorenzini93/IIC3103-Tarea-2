14c7233ba91feb61596a6bd27d44d5481a3d88d2
lorenzini93 <lelorenzini@uc.cl>
2017-03-31 02:27:17 -0300

    Error Heroku

---
+++ railsapi/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.7.1/ext/nokogiri/tmp/x86_64-pc-linux-gnu/ports/libxml2/2.9.4/libxml2-2.9.4/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/lorenzini93/Documents/Universidad/Taller de Integración/Tarea2/railsapi/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.7.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.4/lib"
+XML2_LIBS="-lxml2 -lz     -lm "
+XML2_INCLUDEDIR="-I/home/lorenzini93/Documents/Universidad/Taller de Integración/Tarea2/railsapi/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.7.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.4/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
