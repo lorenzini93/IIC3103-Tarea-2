a4ba5c089b7367893765c52410a015824fbf6045
lorenzini93 <lelorenzini@uc.cl>
2017-03-31 00:20:36 -0300

    Funcionalidades Listas

--- railsapi/app/controllers/api/usuarios_controller.rb
+++ railsapi/app/controllers/api/usuarios_controller.rb
@@ -1,7 +1,48 @@
 class Api::UsuariosController < ApplicationController
   respond_to :json
 
+  # Mostrar usuarios
+  def index
+    usuarios = Usuario.all
+    render json: usuarios
+  end
+
+  # Mostrar usuario
   def show
     respond_with Usuario.find(params[:id])
+  end
+
+  # Crear usuario
+  def create
+   usuario=Usuario.new(user_params) 
+        # if the user is saved successfully than respond with json data and status code 201
+        if usuario.save 
+    render json: usuario, status: 201
+   else
+    render json: { errors: usuario.errors}, status: 422
+   end
+  end
+
+  # Update usuario
+  def update
+    usuario = Usuario.find(params[:id])
+
+    if usuario.update(user_params)
+      render json: usuario, status: 200
+    else
+      render json: { errors: usuario.errors }, status: 422
+    end
+  end
+
+  # Eliminar usuario
+  def destroy
+    usuario = Usuario.find(params[:id])
+    usuario.destroy
+    head 204
+  end
+
+  private
+  def user_params
+   params.require(:usuario).permit(:usuario, :nombre) 
   end
 end  
--- railsapi/config/routes.rb
+++ railsapi/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults:{ format: :json } do
-    resources :usuarios, :only => [:show, :create]
+    resources :usuarios, :only => [:show, :create, :update, :destroy, :index]#, defaults:{ format: :json }
+    get 'usuario/:id', to: 'usuarios#show'
   end 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
